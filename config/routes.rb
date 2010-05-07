963d0f479fad85d97fe7a4e9ee96a619c567483d
Jim Mulholland <jim@jim-mb-pro-2.local>
2010-05-07 10:24:30 -0500

    remove heroku sass and cache

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
 
-  map.heroku_sass_and_cache
   # The priority is based upon order of creation: first created -> highest priority.
   map.root      :controller => "home", :action => "index"
   map.resource  :authentication
